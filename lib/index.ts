9f2e5d0f2ecb8056ca290f33671c284a2fe92f6c
Konrad Gorski <konradgrsk@gmail.com>
2022-04-29 16:04:16 +0200

    refactor: index.ts as iife and collecting args from argv

--- lib/index.ts
+++ lib/index.ts
@@ -1,1 +1,14 @@
-export {default} from './file-type-check';
+#!/usr/bin/env node
+import process from "node:process";
+import fileTypeCheck from "./file-type-check";
+
+(() => {
+  const args = process.argv.slice(2);
+
+  const files = args.filter((file) => /\.(ts|tsx)$/.test(file));
+  if (files.length === 0) {
+    process.exitCode = 0;
+  }
+
+  fileTypeCheck(files);
+})();
