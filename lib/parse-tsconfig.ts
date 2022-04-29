10cf9bf367869cb518aaeb71bf57e0c5a4762a68
Konrad Gorski <konradgrsk@gmail.com>
2022-04-29 16:57:21 +0200

    feat: add tsConfig.json parser

---
+++ lib/parse-tsconfig.ts
@@ -0,0 +1,25 @@
+import type { CompilerOptions } from "typescript";
+
+type TsConfig = {
+  compilerOptions: CompilerOptions;
+  include: string[];
+};
+
+const matchHashComment = new RegExp(
+  /\\"|"(?:\\"|[^"])*"|(\/\/.*|\/\*[\s\S]*?\*\/)/,
+  "gi"
+);
+
+const sanitizeJson = (json: string) =>
+  json.replace(matchHashComment, (match, g) => (g ? "" : match)).trim();
+
+export const parseTsConfig = (data: Buffer) => {
+  const json = sanitizeJson(data.toString("utf8"));
+  try {
+    const tsConfig: TsConfig = JSON.parse(json);
+    return tsConfig;
+  } catch (error) {
+    console.log("Error Unprocessable tsConfig file");
+    process.exit(1);
+  }
+};
