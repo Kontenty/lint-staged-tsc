183790d8f81973c786cae5dacf3c234ca78c9264
Konrad Gorski <konradgrsk@gmail.com>
2022-05-18 11:02:05 +0200

    fix: remove dist folder before build

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,10 +1,15 @@
 import { build } from "esbuild";
+import fs from "node:fs";
 
-build({
-  entryPoints: ["lib/index.ts"],
-  outdir: "dist",
-  bundle: true,
-  platform: "node",
-  target: "node12.2",
-  external: ["node:*"],
-});
+(() => {
+  fs.rmdirSync("./dist/", { recursive: true });
+
+  build({
+    entryPoints: ["lib/index.ts"],
+    outdir: "dist",
+    bundle: true,
+    platform: "node",
+    target: "node12.2",
+    external: ["node:*"],
+  });
+})();
